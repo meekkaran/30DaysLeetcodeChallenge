a9805b8dc373ecb65fdc669247474b5b16ab2743
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-09-21 12:14:16 +0300

    Create BinaryTreeInorder.py
    inorder;;; left-root-right

---
+++ Trees,BinaryTrees,BST/Trees/BinaryTreeInorder.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #for each node, traverse from left to root to right
+        stack = []  #create nodes array to store nodes
+        def inOrder(root):
+            if root is None:
+                return None
+            #visit left
+            inOrder(root.left)
+            #visit base root
+            stack.append(root.val)
+            #viit right
+            inOrder(root.right)
+            
+        inOrder(root)
+        return stack
+
+    
