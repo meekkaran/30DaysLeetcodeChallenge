8d6d67adc1e7b7edbf9ff096a50b18ca32f0e260
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-10-06 07:43:26 +0300

    Create PowerOfThree.py

---
+++ Recursion/PowerOfThree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # a no. is a power of 3 if recursively divided by 3 gives 1
+        # a no. is a power of 3 if its an off no and not less than 0
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        return n % 3 == 0 and self.isPowerOfThree(n/3)
+        
+        
