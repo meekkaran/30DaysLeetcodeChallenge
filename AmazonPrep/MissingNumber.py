2ddbf5af33cbdf2d51fdfac67ba812129e1a07e7
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-11-03 22:00:32 +0300

    Create MissingNumber.py

---
+++ AmazonPrep/MissingNumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        #using o(n) time and space
+        c = Counter(nums)
+        N = len(nums)
+        for i in range(N + 1):
+            if i not in c:
+                return i
+        
+        ##step 2 ; math formula
+        N = len(nums)
+        ExpSum = N*(N+1)//2
+        ActualSum = sum(nums)
+        return ExpSum - ActualSum
