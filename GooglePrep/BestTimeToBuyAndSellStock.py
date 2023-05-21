c71d5d1191824d1c764867444bdde6822fa8d7e5
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2023-05-21 04:26:59 +0300

    Create BestTimeToBuyAndSellStock.py

---
+++ GooglePrep/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,10 @@
+# O(n) time and O(1)space
+class Solution(object):
+    def maxProfit(self, prices):
+        max_profit = 0
+        min_price = float("inf")
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
