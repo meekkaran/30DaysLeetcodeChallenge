81c740d43d19e41f364c77570eaff9b61aad03d2
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2023-05-29 06:26:24 +0300

    Update MissingNumber.py

--- TopInterviewQuestionsLeetcode/MissingNumber.py
+++ TopInterviewQuestionsLeetcode/MissingNumber.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #using o(n) time and space
+        #using o(n) time and O(1) space
         c = Counter(nums)
         N = len(nums)
         for i in range(N + 1):
@@ -8,7 +8,7 @@ class Solution:
                 return i
         
         ##step 2 ; math formula
-        #using o(n) time and space
+        #using o(n) time and 0(1) space
         N = len(nums)
         ExpSum = N*(N+1)//2
         ActualSum = sum(nums)
