c8c4e0453c80d5466dbd91adc70f58a324cc3570
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2023-08-11 06:51:19 +0300

    Update RegularExpressionMatching.py

--- GooglePrep/TopInterviewQuestions-HARD/RegularExpressionMatching.py
+++ GooglePrep/TopInterviewQuestions-HARD/RegularExpressionMatching.py
@@ -15,6 +15,8 @@ class Solution(object):
         for j in range(1, n+1):
             if p[j-1] == "*" :
                 dp[0][j] = dp[0][j-2]
+            else:
+                dp[0][j] = j > 1 and p[j-2] == "*" and dp[0][j-2]
 
         #fill in remaining cells
         for i in range(1, m+1):
@@ -27,7 +29,12 @@ class Solution(object):
                 #two conditions, either 0 occurences or preceding chars
                 # if dp[i][j-2]
                 elif p[j-1] == "*":
-                    dp[i][j] = dp[i][j-2]
-                    if s[i-1] == p[j-2] or p[j-2] == '.':
-                        dp[i][j] = dp[i-1][j]
+                    dp[i][j] = dp[i][j-2] or (p[j-2] == s[i-1] or p[j-2] == '.') and dp[i-1][j]
+                    
+                else:
+                    dp[i][j] = False
         return dp[m][n]
+
+
+
+ 
